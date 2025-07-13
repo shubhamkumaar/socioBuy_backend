dd4ebdaf7367c961b1a53419e0aa4a2603043922
Shubham Kumar <shubhamkumarjhadss8@gmail.com>
2025-07-13 17:32:53 +0530

    refactor: restructure FastAPI app initialization and router inclusion

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from router.user import router as user_router 
 from router.login import router as login_router
 from router.order import router as order_router
 from router.product import router as product_router 
 from router.home import router as home_page
 from router.cart import router as cart_router
-app = FastAPI(prefix="/api")
+app = FastAPI(title="socioBuy API", version="1.0.0")
 
+router = APIRouter(prefix="/api")
 
-app.include_router(home_page)
+router.include_router(home_page)
 
-app.include_router(user_router)
+router.include_router(user_router)
 
-app.include_router(login_router)
+router.include_router(login_router)
 
-app.include_router(order_router)
+router.include_router(order_router)
 
-app.include_router(product_router)
+router.include_router(product_router)
 
-app.include_router(cart_router)
+router.include_router(cart_router)
+
+app.include_router(router)
